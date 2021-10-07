fe4033a72b016d61333487206ce9703c5d6d0d87
weihenaevid <43075344+weihenaevid@users.noreply.github.com>
2021-10-07 13:20:08 -0400

    Create Group_mini-project_1.js
    added nine

--- Group_mini-project_1.js
+++ Group_mini-project_1.js
@@ -6,11 +6,12 @@ let zero = " _  \n| |\n|_|\n";
 let one = "   \n|   \n|  \n";
 let two = " _ \n _|\n|_ \n";
 let three = " _ \n _|\n _|\n";
-let four = "   \n|_|\n  |";
+
 let five = " __\n|__\n __|\n"
-let six = " _  \n|_ \n|_| \n"
+let six = " _  \n_| \n|_| \n"
 let seven = "__\n  |\n  |\n"
 let eight = " _  \n|_| \n|_| \n";
+let nine = " _  \n|_| \n _| \n";
 
 
 
