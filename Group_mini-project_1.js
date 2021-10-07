1ce19740ce0472d59adeab128c85d4714d336f0f
mariam-alzu3 <90797083+mariam-alzu3@users.noreply.github.com>
2021-10-07 17:28:05 -0400

    Update Group_mini-project_1.js

--- Group_mini-project_1.js
+++ Group_mini-project_1.js
@@ -1,23 +1,49 @@
 let zero = " _  \n| |\n|_|\n";
-let one = "   \n|   \n|  \n";
-let two = " _ \n _|\n|_ \n";
-let three = " _ \n _|\n _|\n";
+    let zeroLines = [" _ ","| |","|_|"];
 
-let five = " __\n|__\n __|\n"
-let six = " _  \n_| \n|_| \n"
-let seven = "__\n  |\n  |\n"
+let one = "   \n|   \n|  \n";
+    let oneLines = ["   ","|  ","|  "];
+
+let two = " _ \n _|\n|_ \n";
+    let twoLines = [" _ "," _|","|_ "];
+
+let three = " _ \n _|\n _|\n";
+    let threeLines = [" _ " , " _|" , " _|"];
+
+let four = "   \n|_|\n  |";
+    let fourLines = ["   ", "|_|", "  |"];
+
+let five = " __\n|__\n __|\n";
+    let fiveLines = [" __ ", "|__ ", " __|"];
+
+
 let eight = " _  \n|_| \n|_| \n";
+
+
+let six = " _  \n|_ \n|_| \n"
+
+let seven = "__\n  |\n  |\n"
+
+
 let nine = " _  \n|_| \n _| \n";
 
 
 
+let digitsLines = [zeroLines, oneLines, twoLines, threeLines, fourLines,fiveLines]
 
-console.log(zero);
-console.log(one);
-console.log(two);
-console.log(three);
-console.log(four);
-console.log(five);
-console.log(seven);
-console.log(eight);
-console.log(nine);
+for (let k = 0; k <6; k++){
+    let firstDigit = digitsLines[k]
+    let teen = "";
+    for (let j = 0; j<5; j++){
+
+        ;
+        let secondDigit = digitsLines[j];
+    
+        for (let i = 0; i<3; i++){
+            teen += firstDigit[i] + secondDigit[i];
+            teen += "\n";
+        }
+    }
+    console.log(teen);
+}
+
