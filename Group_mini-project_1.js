a8033aa88432e37adf01082411435cde326b30a5
mariam-alzu3 <90797083+mariam-alzu3@users.noreply.github.com>
2021-10-07 13:13:22 -0400

    Update Group_mini-project_1.js

--- Group_mini-project_1.js
+++ Group_mini-project_1.js
@@ -1,1 +1,21 @@
 console.log("hello");
+
+console.log("hello");
+
+let zero = " _  \n| |\n|_|\n";
+let one = "   \n|   \n|  \n";
+let two = " _ \n _|\n|_ \n";
+let three = " _ \n _|\n _|\n";
+
+let five = " __\n|__\n __|\n"
+
+let seven = "__\n  |\n  |\n"
+let eight = " _  \n|_| \n|_| \n";
+
+
+
+
+console.log(zero);
+console.log(five);
+console.log(seven);
+console.log(eight);
