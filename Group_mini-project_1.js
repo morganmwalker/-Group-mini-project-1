585e50f2697b4cf3f868df83e6c05a147876c811
mariam-alzu3 <90797083+mariam-alzu3@users.noreply.github.com>
2021-10-08 23:26:14 -0400

    Update Group_mini-project_1.js

--- Group_mini-project_1.js
+++ Group_mini-project_1.js
@@ -1,33 +1,13 @@
-let zero = " _  \n| |\n|_|\n";
-    let zeroLines = [" _ ","| |","|_|"];
-
-let one = "   \n|   \n|  \n";
-    let oneLines = ["   ","|  ","|  "];
-
-let two = " _ \n _|\n|_ \n";
-    let twoLines = [" _ "," _|","|_ "];
-
-let three = " _ \n _|\n _|\n";
-    let threeLines = [" _ " , " _|" , " _|"];
-
-let four = "   \n|_|\n  |";
-    let fourLines = ["   ", "|_|", "  |"];
-
-let five = " __\n|__\n __|\n";
-    let fiveLines = [" __ ", "|__ ", " __|"];
-
-let six = " _  \n|_ \n|_| \n"
-    let sixLines = [" _ ", "|_ ", "|_|"];
-
-let seven = "__\n  |\n  |\n";
-    let sevenLines = ["__ ", "  |", "  |"];
-
-let eight = " _  \n|_| \n|_| \n";
-    let eightLines = [" _  ","|_| ","|_| "];
-
-let nine = " _  \n|_| \n _| \n";
-    let nineLines = [" _ ", "|_|", " _|"];
-
+let zeroLines = [" _ ","| |","|_|"];
+let oneLines = ["   ","|  ","|  "];
+let twoLines = [" _ "," _|","|_ "];
+let threeLines = [" _ " , " _|" , " _|"];
+let fourLines = ["   ", "|_|", "  |"];
+let fiveLines = [" __ ", "|__ ", " __|"];
+let sixLines = [" _ ", "|_ ", "|_|"];
+let sevenLines = ["__ ", "  |", "  |"];
+let eightLines = [" _  ","|_| ","|_| "];
+let nineLines = [" _ ", "|_|", " _|"];
 
 
 let digitsLines = [zeroLines, oneLines, twoLines, threeLines, fourLines,fiveLines, sixLines, sevenLines, eightLines, nineLines];
@@ -35,17 +15,17 @@ let digitsLines = [zeroLines, oneLines, twoLines, threeLines, fourLines,fiveLine
 
 let printDoubleDigit = function(tensDigit){
     let innerPrintDoubleDigit = function() {
-        for (let i = 0; i < digitsLines.lenght; i++){
+        for (let i = 0; i < digitsLines.length; i++){
         let twoDigits = "";
         let secDigit = digitsLines[i];
+        
         for (let j = 0; j < 3; j++){
             twoDigits += tensDigit[j] + secDigit[j] + "\n";
         }
         console.log(twoDigits);
     }
 }
-return innerPrintDoubleDigit;
-
+    return innerPrintDoubleDigit;
 }
 
 
@@ -63,6 +43,7 @@ let printNinties = printDoubleDigit(nineLines);
 
 printSingles();
 printTeens();
+printTwenties();
 printThirties();
 printFourties();
 printFifties();
