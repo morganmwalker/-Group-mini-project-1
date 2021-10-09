85fce3089a329f5cf99983912cb1f467e83c5f25
mariam-alzu3 <90797083+mariam-alzu3@users.noreply.github.com>
2021-10-08 23:02:32 -0400

    Update Group_mini-project_1.js

--- Group_mini-project_1.js
+++ Group_mini-project_1.js
@@ -16,34 +16,58 @@ let four = "   \n|_|\n  |";
 let five = " __\n|__\n __|\n";
     let fiveLines = [" __ ", "|__ ", " __|"];
 
+let six = " _  \n|_ \n|_| \n"
+    let sixLines = [" _ ", "|_ ", "|_|"];
+
+let seven = "__\n  |\n  |\n";
+    let sevenLines = ["__ ", "  |", "  |"];
 
 let eight = " _  \n|_| \n|_| \n";
-
-
-let six = " _  \n|_ \n|_| \n"
-
-let seven = "__\n  |\n  |\n"
-
+    let eightLines = [" _  ","|_| ","|_| "];
 
 let nine = " _  \n|_| \n _| \n";
+    let nineLines = [" _ ", "|_|", " _|"];
 
 
 
-let digitsLines = [zeroLines, oneLines, twoLines, threeLines, fourLines,fiveLines]
+let digitsLines = [zeroLines, oneLines, twoLines, threeLines, fourLines,fiveLines, sixLines, sevenLines, eightLines, nineLines];
 
-for (let k = 0; k <6; k++){
-    let firstDigit = digitsLines[k]
-    let teen = "";
-    for (let j = 0; j<5; j++){
 
-        ;
-        let secondDigit = digitsLines[j];
-    
-        for (let i = 0; i<3; i++){
-            teen += firstDigit[i] + secondDigit[i];
-            teen += "\n";
+let printDoubleDigit = function(tensDigit){
+    let innerPrintDoubleDigit = function() {
+        for (let i = 0; i < digitsLines.lenght; i++){
+        let twoDigits = "";
+        let secDigit = digitsLines[i];
+        for (let j = 0; j < 3; j++){
+            twoDigits += tensDigit[j] + secDigit[j] + "\n";
         }
+        console.log(twoDigits);
     }
-    console.log(teen);
 }
+return innerPrintDoubleDigit;
+
+}
+
+
+let printSingles = printDoubleDigit(zeroLines);
+let printTeens = printDoubleDigit(oneLines);
+let printTwenties = printDoubleDigit(twoLines);
+let printThirties = printDoubleDigit(threeLines);
+let printFourties = printDoubleDigit(fourLines);
+let printFifties = printDoubleDigit(fiveLines);
+let printSixties = printDoubleDigit(sixLines);
+let printSeventies = printDoubleDigit(sevenLines);
+let printEighties = printDoubleDigit(eightLines);
+let printNinties = printDoubleDigit(nineLines);
+
+
+printSingles();
+printTeens();
+printThirties();
+printFourties();
+printFifties();
+printSixties();
+printSeventies();
+printEighties();
+printNinties();
 
